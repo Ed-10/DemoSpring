af89a3eaf54c61390fd18b6d1f4d33c5fbc13f3b
eduardo.martinez <eduardomartinez67@aragon.unam.mx>
2022-10-04 14:46:57 -0500

    PrimeraParte

---
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@RestController
+@Slf4j
+public class ControladorInicio {
+	@GetMapping("/")
+	public String inicio() {
+		System.out.println("ejecutando el controlador Rest");
+		return "Hola mundo con Spring c:";
+	}
+
+}
